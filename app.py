75bb67a1a5fd4635805e2bfc3673f806792c5427
rakesh2971 <telangrc2971@gmail.com>
2025-12-30 12:15:05 +0530

    project initialization

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'hiretrack-secret-key'
+jwt = JWTManager(app)
+
+@app.route('/')
+def home():
+    return {"message": "hiretrack is working!"}
+
+if __name__ == '__main__':
+    app.run(debug=True)
